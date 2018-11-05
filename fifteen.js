7d101d9b10e1924e2ef86d87ba4d27a281169891
Shiloh Waite Dacosta <shilohwaite@gmail.com>
2018-11-05 18:58:42 -0500

    Place tiles in the correct order

---
+++ fifteen.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+    var step= document.querySelectorAll("#puzzlearea div");
+    var piece= document.querySelectorAll(".puzzlepiece");
+    var x =0;
+    var y =0;
+    var TOP = "300px";
+    var LEFT = "300px";
+    var bTop, bleft;
+    var count = 0;
+
+    // position of tile
+    for (let index =0; index< step.length; index ++){
+        step[index].style.backgroundImage= "url(background.jpg)";
+        step[index].setAttribute("class", "puzzlepiece");
+        step[index].style.left = x + "px";
+        step[index].style.top = y + "px";
+        step[index].style.backgroundPosition = "-" + x + "px"+ "-"+ y + "px";
+        if (x<300){
+            x+=100;
+        }else{
+            x=0;
+            y+=100;
+        }
+    }
+    
+   
+
+  
+}
